2c2776c7451d098d77eab4737655fc038fcea175
tom <tom.hengl@isric.org>
2017-09-15 09:11:36 +0200

    fixed join country names

--- grids/countries/GAUL_250m.R
+++ grids/countries/GAUL_250m.R
@@ -1,6 +1,8 @@
-## Rasterize GAUL
-## http://www.fao.org/geonetwork/srv/en/metadata.show?currTab=simple&id=12691
+## Rasterize GAUL (http://www.fao.org/geonetwork/srv/en/metadata.show?currTab=simple&id=12691) and mask out water bodies
+## The GAUL dataset is distributed to the United Nations and other authorized international and national institutions/agencies. FAO grants a license to use, download and print the materials contained in the GAUL dataset solely for non-commercial purposes and in accordance with the conditions specified in the data license. The full GAUL Data License document is available for downloading.
+## tom.hengl@isric.org
 
+setwd("/data/countries")
 library(raster)
 library(rgdal)
 library(foreign)
@@ -22,15 +24,33 @@ gaul.tbl = read.dbf("/mnt/cartman/FAO/GAUL/g2015_2014_1/g2015_2014_1.dbf")
 ## 3422 rows
 str(gaul.tbl)
 str(levels(gaul.tbl$ADM1_NAME))
-#summary(as.factor(gaul.tbl$ADM1_CODE))
 ## 3218
-gaul.tbl$Value = as.integer(as.factor(gaul.tbl$ADM1_NAME))
+## There are many duplicated names unfortunately
+summary(duplicated(gaul.tbl$ADM1_NAME))
+gaul.tbl$ADM0_ADM1 = as.factor(make.unique(paste(gaul.tbl$ADM0_NAME, gaul.tbl$ADM1_NAME, sep="_")))
+summary(duplicated(gaul.tbl$ADM0_ADM1))
+## 276 'countries'
+#summary(as.factor(gaul.tbl$ADM1_CODE))
+gaul.tbl$Value = as.integer(gaul.tbl$ADM0_ADM1)
+summary(gaul.tbl$Value)
 write.dbf(gaul.tbl, "/mnt/cartman/FAO/GAUL/g2015_2014_1/g2015_2014_1.dbf")
-hb.leg = data.frame(Number=1:length(levels(as.factor(gaul.tbl$ADM1_NAME))), ADM1_NAME=levels(as.factor(gaul.tbl$ADM1_NAME)))
-hb.leg$Value = plyr::join(hb.leg, gaul.tbl, match="first")$Value
-hb.leg$ADM0_NAME = plyr::join(hb.leg, gaul.tbl, match="first")$ADM0_NAME
-hb.leg$ADM0_NAME_filename = normalizeFilename(iconv(hb.leg$ADM0_NAME, to="ASCII", sub=""))
-hb.leg$ADM1_NAME_filename = normalizeFilename(paste(iconv(hb.leg$ADM0_NAME, to="ASCII", sub=""), iconv(hb.leg$ADM1_NAME, to="ASCII", sub=""), sep="__"))
+hb.leg = gaul.tbl[,c("Value","ADM0_NAME","ADM1_NAME")]
+## https://gist.github.com/primaryobjects/b56189e52a3f0e3cfdbf
+friendlyUrl <- function(text, sep = '-', max = 80) {
+  # Replace non-alphanumeric characters.
+  url <- gsub('[^A-Za-z0-9]', sep, text)
+  # Remove double separators (do this twice, in case of 4 or 3 repeats).
+  doubleSep <- paste(sep, sep, sep = '')
+  url <- gsub(doubleSep, sep, url)
+  url <- gsub(doubleSep, sep, url)
+  # Convert to lowercase and trim to max length.
+  url <- substr(tolower(url), 1, max)
+  # Trim leading and trailing separators.
+  gsub('^-+|-$', '', url)
+}
+## URL friendly names:
+hb.leg$ADM0_NAME_filename = friendlyUrl(iconv(hb.leg$ADM0_NAME, to="ASCII", sub=""))
+hb.leg$ADM1_NAME_filename = friendlyUrl(paste(iconv(hb.leg$ADM0_NAME, to="ASCII", sub=""), iconv(hb.leg$ADM1_NAME, to="ASCII", sub=""), sep="__"))
 hb.leg$ADM0_NAME_Value = as.integer(hb.leg$ADM0_NAME)
 View(hb.leg)
 write.csv(hb.leg, "g2015_2014_1_legend.csv")
@@ -56,19 +76,25 @@ fun_mask <- function(i, tiles, dir="./tiled/", lc="/mnt/cartman/stacked250m/LCEE
     x$mask = ifelse(x$band1==0|x$band1==210|is.na(x$band1), NA, x$gaul)
     x$mask2 = plyr::join(data.frame(Value=x$mask), hb.leg[,c("Value","ADM0_NAME_Value")], match="first")$ADM0_NAME_Value
     if(!all(is.na(x$mask))){
-      writeGDAL(x["mask"], type="Int16", mvFlag = -32768, out.tif, options=c("COMPRESS=DEFLATE"))
-      writeGDAL(x["mask2"], type="Int16", mvFlag = -32768, gsub("T", "M", out.tif), options=c("COMPRESS=DEFLATE"))
+      writeGDAL(x["mask"], type="Int16", mvFlag=-32768, out.tif, options=c("COMPRESS=DEFLATE"))
+      writeGDAL(x["mask2"], type="Int16", mvFlag=-32768, gsub("T", "M", out.tif), options=c("COMPRESS=DEFLATE"))
     }
   }
 }
 
 #dir.create("./tiled")
+#unlink(list.files("./tiled", pattern=".tif", full.names=TRUE))
 fun_mask(i=801, tiles=tiles, hb.leg=hb.leg)
 library(parallel)
 x0 = mclapply(1:nrow(tiles), FUN=fun_mask, tiles=tiles, hb.leg=hb.leg)
 ## Mosaick back results of computing:
 t.lst <- list.files(path="./tiled", pattern=glob2rx("^T*.tif$"), full.names=TRUE, recursive=TRUE)
+## 1651
 cat(t.lst, sep="\n", file="mask_tiles.txt")
 system('gdalbuildvrt -input_file_list mask_tiles.txt mask.vrt')
-system(paste0('gdalwarp mask.vrt GAUL_ADMIN1_250m.tif -ot \"Int16\" -dstnodata -32768 -co \"BIGTIFF=YES\" -r \"near\" -overwrite -co \"COMPRESS=DEFLATE\" -te ', xllcorner, ' ', yllcorner, ' ', xucorner, ' ', yucorner))
-
+system(paste0('gdalwarp mask.vrt GAUL_ADMIN1_landmask_250m.tif -ot \"Int16\" -dstnodata -32768 -co \"BIGTIFF=YES\" -r \"near\" -overwrite -co \"COMPRESS=DEFLATE\" -te ', xllcorner, ' ', yllcorner, ' ', xurcorner, ' ', yurcorner))
+## Countries:
+m.lst <- list.files(path="./tiled", pattern=glob2rx("^M*.tif$"), full.names=TRUE, recursive=TRUE)
+cat(m.lst, sep="\n", file="mask2_tiles.txt")
+system('gdalbuildvrt -input_file_list mask2_tiles.txt mask2.vrt')
+system(paste0('gdalwarp mask2.vrt GAUL_ADMIN0_landmask_250m.tif -ot \"Int16\" -dstnodata -32768 -co \"BIGTIFF=YES\" -r \"near\" -overwrite -co \"COMPRESS=DEFLATE\" -te ', xllcorner, ' ', yllcorner, ' ', xurcorner, ' ', yurcorner))
